6efc4a6265ac40a4474a2bec7e7057745fef1f69
Sarveshwar Senthil Kumar <sarveshwar313@gmail.com>
2025-04-01 20:54:15 -0400

    Improved CreateDatabase.py

--- createDatabase.py
+++ createDatabase.py
@@ -6,7 +6,35 @@ database.truncate(0)
 database.close()
 connection = sqlite3.connect("users.db")
 crsr = connection.cursor()
-crsr.execute("CREATE TABLE users (id INTEGER, username NOT NULL, password NOT NULL, emailaddress, name, dateJoined, salt, accountStatus, role, lastLogin, twoFactorAuth, PRIMARY KEY(id))")
+
+fields = [
+    #Personal Information for Account Setup and Maintenance
+          "username NOT NULL", 
+          "password NOT NULL",
+          "dateJoined",
+          "salt",
+          "accountStatus",
+          "role", #hierarchy for possible admins
+          "twoFactorAuth",
+          "lastLogin",
+          "emailAddress",
+          "phoneNumber",
+          "name", #Full Name
+          "dateOfBirth",
+          "gender", #Prefer Not to Say or Other
+        ]
+
+
+#Easily converatble to MySQL or other databases due to iterative strategy as opposed to hardcoding the db create string, also improves readability and ease of maintenance and adding new fields
+
+dbCreateString = "CREATE TABLE users (id INTEGER, "
+
+for field in fields:
+    dbCreateString += field+", "
+
+dbCreateString+="PRIMARY KEY(id))"
+
+crsr.execute(dbCreateString)
 connection.commit()
 crsr.close()
 connection.close()
